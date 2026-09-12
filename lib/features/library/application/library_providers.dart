a41ce7c7fb4d5d29c9e763c3845b7af885365866
Hassan <hassanoft@yahoo.com>
2026-09-12 15:12:50 +0000

    Update library_providers.dart

--- lib/features/library/application/library_providers.dart
+++ lib/features/library/application/library_providers.dart
@@ -1,9 +1,16 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'library_controller.dart';
 
-final libraryControllerProvider = ChangeNotifierProvider<LibraryController>((ref) {
+final libraryControllerProvider =
+    ChangeNotifierProvider<LibraryController>((ref) {
   final controller = LibraryController();
+
   controller.init();
-  ref.onDispose(controller.dispose);
+
+  ref.onDispose(
+    controller.dispose,
+  );
+
   return controller;
 });
